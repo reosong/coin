ef026bdaf9ba27f510e53579b8ae5016b26d486b
reosong <ambition_1018@naver.com>
2022-11-22 13:12:02 +0900

    first commit

---
+++ coinplus/src/test/java/com/example/coinplus/CoinplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coinplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
